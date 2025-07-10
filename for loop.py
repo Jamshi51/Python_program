0da308a59d87ad42eea256769416e4abaffc6a95
Jamshi51 <jamshi5171@gmail.com>
2025-07-10 13:45:27 +0530

    updated version in day 12

---
+++ for loop.py
@@ -0,0 +1,49 @@
+#list
+# l=[23,24,3,5,45]
+# # for k in list:
+# #     # #print(k)  23
+# #     #            24
+# #     #            3
+# #     #            5
+# #     #            45
+# #     # print(k,end=" ") #23,24,3,5,45
+# print(list(enumerate(l,1))) #[(1, 23), (2, 24), (3, 3), (4, 5), (5, 45)]
+# for c,k in enumerate(l,6):
+#     print(c,k) #output->0 23
+                        # 1 24
+                        # 2 3
+                        # 3 5
+                        # 4 45
+
+#sring
+# s="jamshi"
+# for m in s:
+#     print(m,end=" ")
+
+#dictionary
+# d={"name":"jamshi","age":23,"hobby":["art","coding"]}
+# # for w in d:
+# #     print(d[w],end=" ") #jamshi 23 ['art', 'coding']
+# # print(" ") #break the output
+# # for w in d.items(): #('name', 'jamshi') ('age', 23) ('hobby', ['art', 'coding']) 
+# #     print(w,end=" ")
+# # for w in d.values(): # only values print ,jamshi 23 ['art', 'coding'] 
+# #     print(w,end=" ")
+# # for w in d.keys():  #only keys print
+# #     print(w,end=" ")
+# # for w,value in d.items(): # name jamshi
+# #                             # age 23
+# #                             # hobby ['art', 'coding']
+# #     print(w,value)
+
+#     #OR
+# for w in d:
+#     print(w,d[w]) # same output
+
+# #tuple
+t=(1,2 ,2,"jamshi","python")
+for k in t:
+    print(k,end=" ") #1 2 2 jamshi python
+
+
+
---
+++ range.py
@@ -0,0 +1,14 @@
+# print(list(range(1,34,2))) #[1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33]
+# for k in range(1,10):
+#     print(k)
+
+#multiplication table
+# n=int(input("enter the nymber :"))
+# for k in range(1,11):
+#     #print(f"{k}*{n}={k*n}")  #multiplication of 5
+#     print(k*n) # 3,6,9,12,15,18,21,24,27,30
+
+for num in range(1,17):
+    if num%2==0:
+        print(num)
+    
