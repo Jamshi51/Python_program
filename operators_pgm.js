47551f7dbeb0ce62b03c0741e13766be89c74ad1
Jamshi51 <jamshi5171@gmail.com>
2025-08-27 13:50:07 +0530

    Javascript

---
+++ operators_pgm.js
@@ -0,0 +1,21 @@
+//Arithmatic
+// let a=10,b=13
+// console.log("a+b=",a+b)
+// console.log("a-b=",a-b)
+// console.log("a*b=",a*b)
+// console.log("a/b=",a/b)
+// console.log("a%b=",a%b)
+// console.log("a**b=",a**b)
+// console.log("++a=",++a)
+// console.log("--b=",--b)
+
+//Assignment
+let c=10
+console.log("a=",a+=2)
+console.log("a=",a-=2)
+console.log("a=",a*=2)
+console.log("a=",a/=2)
+console.log("a=",a**=2)
+
+
+
